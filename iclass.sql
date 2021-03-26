a4e625f10ed88510f1406019c9db2a772cbf6a4e
erikrihendri <erikrihendri>
2021-03-26 08:41:24 +0700

    sql

--- iclass.sql
+++ iclass.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 18, 2021 at 06:51 AM
+-- Generation Time: Mar 26, 2021 at 02:40 AM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -20,6 +20,73 @@ SET time_zone = "+00:00";
 --
 -- Database: `iclass`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `nama`, `username`, `password`) VALUES
+(1, 'erik', 'erik', 'erik');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `start_event` datetime NOT NULL,
+  `end_event` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `title`, `start_event`, `end_event`) VALUES
+(1, 'sdsadas', '2021-03-03 00:00:00', '2021-03-04 00:00:00'),
+(2, 'coba', '2021-03-26 06:01:56', '2021-03-26 06:01:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `link-meeting` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id`, `nama`, `link-meeting`, `created_at`, `deleted_at`, `updated_at`) VALUES
+(1, '326', 'https://zoom.us/', '2021-03-26 00:12:15', '2021-03-26 00:12:15', '2021-03-26 00:12:15'),
+(2, '316', 'https://zoom.us/', '2021-03-26 00:12:15', '2021-03-26 00:12:15', '2021-03-26 00:12:15'),
+(4, 'sadsadsad', 'asdsad', '2021-03-26 01:02:47', '2021-03-26 01:02:47', '2021-03-26 01:02:47'),
+(5, 'sadsadsad', 'asdsad', '2021-03-26 01:03:03', '2021-03-26 01:03:03', '2021-03-26 01:03:03'),
+(7, 'sdlsakdmasldaslkdmk', 'dsadsadsadad', '2021-03-26 01:07:14', '2021-03-26 01:07:14', '2021-03-26 01:07:14');
 
 -- --------------------------------------------------------
 
@@ -49,12 +116,29 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nama`, `kelas_id`, `jurusan`, `pilih-paket`, `telepon`, `email`, `username`, `password`, `bukti-pembayaran`, `status`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(13, 'erik', 0, 'dfdsfsfsd', 3, '1234567890', 'erca.rihendri@gmail.com', 'user2', '$2y$10$JGr4vxGCKlbRPdVzDoNhguOIcxbwLJF22ToGZ2HOa8I3a3xpu7JGy', '', 0, '2021-03-15 09:31:05', '2021-03-15 09:31:05', '2021-03-15 09:31:05'),
-(14, 'sad', 0, '12345', 1, '12345678', '221810270@stis.ac.id', '1234567890', '$2y$10$WcgjXbFsx9X/Z7.W1KKdFeAJNsXbpqfVQKhIk2xHQv7pGQJ2jOSw.', '', 0, '2021-03-15 09:33:16', '2021-03-15 09:33:16', '2021-03-15 09:33:16');
+(16, 'erik', 0, 'erik', 1, '09876543', '221810270@stis.ac.id', 'erca2005', '$2y$10$QYUYjLDOF.o4fwCaOA/6iOnVGtQ0MayXWT1aX4J3Xl3U1YCoBLtgG', '', 0, '2021-03-25 19:14:35', '2021-03-25 19:14:35', '2021-03-25 19:14:35');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -67,10 +151,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
