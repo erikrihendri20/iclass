de31f7bf624d2377f7d9deda9d496377b3ba16fb
Xeryus <akhmadfadilm@gmail.com>
2021-04-13 20:42:19 +0700

    fix authFilter

--- iclass/app/Filters/AuthFilter.php
+++ iclass/app/Filters/AuthFilter.php
@@ -10,20 +10,19 @@ class AuthFilter implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
-        if(session()->log!=true){
-            $flash='<div class="alert alert-danger alert-dismissible fade show" role="alert">
+        if (session()->log != true) {
+            $flash = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
                             silahkan masuk terlebih dahulu
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>';
-                session()->setFlashdata('flash' , $flash);
-                return redirect()->to('masuk');
+            session()->setFlashdata('flash', $flash);
+            return redirect()->to(base_url('masuk'));
         }
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
-        
     }
 }
