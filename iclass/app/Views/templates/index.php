4c84d0ca0770c181213e3a32066c900acdef2a08
Xeryus <akhmadfadilm@gmail.com>
2021-05-04 18:34:17 +0700

    fix font

--- iclass/app/Views/templates/index.php
+++ iclass/app/Views/templates/index.php
@@ -16,8 +16,6 @@
     <script src="https://lipis.github.io/bootstrap-sweetalert/dist/sweetalert.js"></script>
     <link rel="stylesheet" href="https://lipis.github.io/bootstrap-sweetalert/dist/sweetalert.css" />
     <link rel="stylesheet" href="/css/font.css">
-    <!-- <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet"> -->
 
     <!-- <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script> -->
     <title>Document</title>
